87ce833d2ce11ba529c48f122e3a364eb288f4cc
Vital Hegner <vhegner@dotworks.ch>
2021-11-23 12:05:15 +0100

    added requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import googlebooks
-from pprint import pprint
+
 
 def test():
 
